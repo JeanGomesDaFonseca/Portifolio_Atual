26b8b3e43336cc0eae27857eae946ea2c123fb4e
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2023-01-31 20:29:09 -0300

    first commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,33 @@
+import styles from './Banner.module.css';
+import circuloColorido from 'assets/circulo_colorido.png';
+import minhaFoto from 'assets/minhaFoto.jpeg'
+
+export default function Banner() {
+    return (
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Helo Guys!
+                </h1>
+
+                <p className={styles.paragrafo}>
+                    Boas vindas ao meu espaço pessoal! Eu sou Jean Gomes, estagiário de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)
+                </p>
+            </div>
+
+            <div className={styles.imagens}>
+                <img
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    aria-hidden={true}
+                />
+
+                <img
+                    className={styles.minhaFoto}
+                    src={minhaFoto}
+                    alt="Foto do Antônio Evaldo sorrindo"
+                />
+            </div>
+        </div>
+    )
+}
---
+++ src/paginas/Post/index.js
@@ -0,0 +1,32 @@
+import './Post.css';
+
+import { useParams } from "react-router-dom"
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
+
+export default function Post() {
+    const parametros = useParams();
+
+    const post = posts.find((post) => {
+        return post.id === Number(parametros.id);
+    })
+
+    if (!post) {
+        return <h1>Post não encontrado...</h1>
+    }
+
+    return (
+        <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+        >
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+            
+        </PostModelo>
+    )
+}
