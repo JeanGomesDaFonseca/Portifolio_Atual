c63c86d1875283e2ef21420e3107e5c88fa60965
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2023-01-31 20:35:20 -0300

    alterações

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -24,21 +24,23 @@ export default function SobreMim() {
                 Oi, tudo bem? Eu sou estudante de Front-end na Alura e estou feliz de te ver por aqui.
             </p>
             <p className={styles.paragrafo}>
-                Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.
+                Formado na área administrativa como técnico, tive oportunidades de conhecer as áreas de produção e recepção, onde pude aprimorar minhas habilidades de comunicação e organização.</p>
+            <p className={styles.paragrafo}>
+                Atualmente, estou estagiando na empresa Dguests como Desenvolvedor Web e Mobile "Front-End" e estou desenvolvendo meus conhecimentos cursando Sistema da Informação na Universidade Anhembi Morumbi e pela plataforma da Alura, por onde venho estudado com bastante frequência React.js. </p>
+            <p className={styles.paragrafo}>
+                Dentre outras características minhas, gostaria de destacar a facilidade com softwares e hardwares, a adaptabilidade, minha capacidade analítica em diferentes situações e o trabalho em equipe.             </p>
+            <p className={styles.paragrafo}>
+                Possuo conhecimento em ferramentas como:
             </p>
             <p className={styles.paragrafo}>
-                No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.
-            </p>
-            <p className={styles.paragrafo}>
-                Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.
-            </p>
-            <p className={styles.paragrafo}>
-                E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.
+                - React.js,
+                - Node.js,
+                - HTML semântico,
+                - CSS / SCSS responsivo,
+                - JavaScript / Typescript,
+                - Git / GitHub.
 
-            </p>
-            <p className={styles.paragrafo}>
-                Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!
-            </p>
+                #Front-End #React #JavaScript #HTML #CSS #SASS #Git #Gihub            </p>
         </PostModelo>
     )
 }
