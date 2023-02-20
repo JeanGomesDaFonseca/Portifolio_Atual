a5932038cae3b13c2e3d1658c50cd44794e9a209
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2023-02-20 18:07:44 -0300

    Atualizações do projeto, alterando cards e adicionando meus projetos

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -2,7 +2,7 @@ import styles from "./SobreMim.module.css";
 
 import PostModelo from "componentes/PostModelo";
 import fotoCapa from "assets/sobre_mim_capa.png";
-import fotoSobreMim from "assets/minhaFoto.jpeg";
+import fotoSobreMim from "assets/FotoCapa.jpeg";
 
 export default function SobreMim() {
     return (
@@ -11,7 +11,7 @@ export default function SobreMim() {
             titulo="Sobre mim"
         >
             <h3 className={styles.subtitulo}>
-                Olá, eu sou o Jean!
+                Olá, meu nome é Jean!
             </h3>
 
             <img
@@ -21,26 +21,18 @@ export default function SobreMim() {
             />
 
             <p className={styles.paragrafo}>
-                Oi, tudo bem? Eu sou estudante de Front-end na Alura e estou feliz de te ver por aqui.
+                Sou um profissional formado na área administrativa como técnico e tenho experiência nas áreas de produção e recepção,
+                onde pude aperfeiçoar minhas habilidades de comunicação e organização.
+                Atualmente além de estudante de Sistema da Informação na Universidade Anhembi Morumbi e pela plataforma da Alura,
+                também sou estagiário na empresa Dguests na função de Desenvolvedor Web "Front-End". Estou constantemente me atualizando e ampliando meus conhecimentos em React.js.
             </p>
             <p className={styles.paragrafo}>
-                Formado na área administrativa como técnico, tive oportunidades de conhecer as áreas de produção e recepção, onde pude aprimorar minhas habilidades de comunicação e organização.</p>
+                Dentre minhas características destaco a facilidade com softwares e hardwares,
+                capacidade de adaptação, minha analítica em diferentes situações e o trabalho em equipe.
+                Possuo conhecimento em React.js, Node.js, HTML semântico, CSS/SCSS responsivo, JavaScript/Typescript, Git/GitHub.</p>
             <p className={styles.paragrafo}>
-                Atualmente, estou estagiando na empresa Dguests como Desenvolvedor Web e Mobile "Front-End" e estou desenvolvendo meus conhecimentos cursando Sistema da Informação na Universidade Anhembi Morumbi e pela plataforma da Alura, por onde venho estudado com bastante frequência React.js. </p>
-            <p className={styles.paragrafo}>
-                Dentre outras características minhas, gostaria de destacar a facilidade com softwares e hardwares, a adaptabilidade, minha capacidade analítica em diferentes situações e o trabalho em equipe.             </p>
-            <p className={styles.paragrafo}>
-                Possuo conhecimento em ferramentas como:
-            </p>
-            <p className={styles.paragrafo}>
-                - React.js,
-                - Node.js,
-                - HTML semântico,
-                - CSS / SCSS responsivo,
-                - JavaScript / Typescript,
-                - Git / GitHub.
-
-                #Front-End #React #JavaScript #HTML #CSS #SASS #Git #Gihub            </p>
+                Sempre busco novos desafios e oportunidades para ampliar meus conhecimentos,
+                a fim de me desenvolver cada vez mais e alcançar tanto o sucesso pessoal quanto profissional . </p>
         </PostModelo>
     )
 }
