c310b00bd8dbe5287ddc70f3bc2be5fdb01ee0f3
Jean Gomes <jgomesfonseca21@gmail.com>
2023-09-14 20:23:28 -0300

    Update index.js

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -11,7 +11,7 @@ export default function Banner() {
                 </h1>
 
                 <p className={styles.paragrafo}>
-                    Boas vindas ao meu espaço pessoal! Eu sou Jean Gomes, estagiário de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)
+                    Boas vindas ao meu espaço pessoal! Eu sou Jean Gomes, estagiário de Fullstack da Dguests. Aqui compartilho vários conhecimentos, espero que gostem :)
                 </p>
             </div>
 
